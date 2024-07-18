c97b7919e27fc2210f93490157c9529982058922
Crtomas2 <charlietomas88@gmail.com>
2024-07-18 12:53:56 +0800

    test deployed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'; // Import ReactDOM correctly
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchByLocation from './components/MainPage';
+import Contact from './components/Contact';
+import AboutUS from './components/About';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <Router basename="/WebDevExam-Assesment">
+      <div className="App">
+        <header className="App-header">
+          {/* Navigation code if needed */}
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<SearchByLocation />} />
+            <Route path="/About" element={<AboutUS />} />
+            <Route path="/Contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root')); // Render your app using ReactDOM
+
+export default App;
