0c16ed8163a1ae2675e49940f173a413b9e1b767
Crtomas2 <charlietomas88@gmail.com>
2024-07-18 10:24:52 +0800

    Added other files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-// import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchByLocation from './components/MainPage';
 import Contact from './components/Contact';
 import AboutUS from './components/About';
 
-
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {/* <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
-            </ul>
-          </nav> */}
+          {/* Navigation code if needed */}
         </header>
         <main>
           <Routes>
             <Route path="/" element={<SearchByLocation />} />
             <Route path="/About" element={<AboutUS />} />
             <Route path="/Contact" element={<Contact />} />
-
-            {/* <Route path="/contact" element={<Contact />} />
-            <Route path="/test" element={<HomePage />} /> */}
-           
           </Routes>
         </main>
       </div>
